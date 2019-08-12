e896f4c28e45d1a4bff27df4e781ba404e8aebf5
keigo <k5.trismegistus@gmail.com>
2019-08-12 19:57:30 +0900

    :construction: WIP separate recognition code

--- lib/main.dart
+++ lib/main.dart
@@ -4,25 +4,11 @@ import 'package:pembroke/pages/card_list_page.dart';
 import 'package:speech_recognition/speech_recognition.dart';
 import 'package:simple_permissions/simple_permissions.dart';
 import 'package:pembroke/repositories/db.dart';
+import 'package:pembroke/constants/constants.dart';
 
 void main() {
   DbStore.initialize()
     .then((_) => runApp(new MyApp()));
-}
-
-const languages = const [
-  const Language('Francais', 'fr_FR'),
-  const Language('English', 'en_US'),
-  const Language('Pусский', 'ru_RU'),
-  const Language('Italiano', 'it_IT'),
-  const Language('Español', 'es_ES'),
-];
-
-class Language {
-  final String name;
-  final String code;
-
-  const Language(this.name, this.code);
 }
 
 class MyApp extends StatelessWidget {
@@ -40,49 +26,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  SpeechRecognition _speech;
-
-  bool _speechRecognitionAvailable = false;
-  bool _isListening = false;
-
-  String transcription = '';
-
-  //String _currentLocale = 'en_US';
-  Language selectedLang = languages.first;
-
-  initState() {
-    super.initState();
-
-    activateSpeechRecognizer();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  void activateSpeechRecognizer() async {
-    print('_MyAppState.activateSpeechRecognizer... ');
-    _speech = new SpeechRecognition();
-    _speech.setAvailabilityHandler(onSpeechAvailability);
-    _speech.setCurrentLocaleHandler(onCurrentLocale);
-    _speech.setRecognitionStartedHandler(onRecognitionStarted);
-    _speech.setRecognitionResultHandler(onRecognitionResult);
-    _speech.setRecognitionCompleteHandler(onRecognitionComplete);
-    // _speech.setErrorHandler(errorHandler);
-
-    await requirePermission();
-
-    var res = await _speech.activate();
-    setState(() {
-      _speechRecognitionAvailable = res;
-      print(res);
-    });
-  }
-
-  void requirePermission() async {
-    if (!await SimplePermissions.checkPermission(Permission.RecordAudio)) {
-      SimplePermissions.requestPermission(Permission.RecordAudio);
-    } else {
-      print("ok");
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -90,12 +33,6 @@ class _HomeScreenState extends State<HomeScreen> {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('SpeechRecognition'),
-        actions: [
-          new PopupMenuButton<Language>(
-            onSelected: _selectLangHandler,
-            itemBuilder: (BuildContext context) => _buildLanguagesWidgets,
-          )
-        ],
       ),
       body: new Padding(
           padding: new EdgeInsets.all(8.0),
@@ -104,31 +41,6 @@ class _HomeScreenState extends State<HomeScreen> {
               mainAxisSize: MainAxisSize.min,
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                new Expanded(
-                    child: new Container(
-                        padding: const EdgeInsets.all(8.0),
-                        color: Colors.grey.shade200,
-                        child: new Text(transcription))),
-                _buildButton(
-                  onPressed: _speechRecognitionAvailable && !_isListening
-                      ? () => start()
-                      : null,
-                  label: _isListening
-                      ? 'Listening...'
-                      : 'Listen (${selectedLang.code})',
-                ),
-                _buildButton(
-                  onPressed: _isListening ? () => cancel() : null,
-                  label: 'Cancel',
-                ),
-                _buildButton(
-                  onPressed: _isListening ? () => stop() : null,
-                  label: 'Stop',
-                ),
-                _buildButton(
-                  onPressed: requirePermission,
-                  label: 'Permission',
-                ),
                 _buildButton(
                   onPressed: () {
                     Navigator.push(
@@ -153,18 +65,6 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  List<CheckedPopupMenuItem<Language>> get _buildLanguagesWidgets => languages
-      .map((l) => new CheckedPopupMenuItem<Language>(
-            value: l,
-            checked: selectedLang == l,
-            child: new Text(l.name),
-          ))
-      .toList();
-
-  void _selectLangHandler(Language lang) {
-    setState(() => selectedLang = lang);
-  }
-
   Widget _buildButton({String label, VoidCallback onPressed}) => new Padding(
       padding: new EdgeInsets.all(12.0),
       child: new RaisedButton(
@@ -175,32 +75,4 @@ class _HomeScreenState extends State<HomeScreen> {
           style: const TextStyle(color: Colors.white),
         ),
       ));
-
-  void start() => _speech
-      .listen(locale: selectedLang.code)
-      .then((result) => print('_MyAppState.start => result $result'));
-
-  void cancel() =>
-      _speech.cancel().then((result) => setState(() => _isListening = result));
-
-  void stop() => _speech.stop().then((result) {
-        setState(() => _isListening = result);
-      });
-
-  void onSpeechAvailability(bool result) =>
-      setState(() => _speechRecognitionAvailable = result);
-
-  void onCurrentLocale(String locale) {
-    print('_MyAppState.onCurrentLocale... $locale');
-    setState(
-        () => selectedLang = languages.firstWhere((l) => l.code == locale));
-  }
-
-  void onRecognitionStarted() => setState(() => _isListening = true);
-
-  void onRecognitionResult(String text) => setState(() => transcription = text);
-
-  void onRecognitionComplete() => setState(() => _isListening = false);
-
-  void errorHandler() => activateSpeechRecognizer();
 }
---
+++ lib/utils/voice_recognizer.dart
@@ -0,0 +1,55 @@
+import 'package:speech_recognition/speech_recognition.dart';
+import 'package:simple_permissions/simple_permissions.dart';
+import 'package:pembroke/constants/constants.dart';
+
+class VoiceRecognizerStore {
+  static VoiceRecognizer _voice_recognizer;
+}
+
+class VoiceRecognizer {
+  bool _voice_regcognition_permitted = false;
+  bool _speechRecognitionAvailable = false;
+  bool _isListening = false;
+
+  SpeechRecognition _speech;
+
+  Future<bool> requirePermission() async {
+    if (!await SimplePermissions.checkPermission(Permission.RecordAudio)) {
+      var res = await SimplePermissions.requestPermission(Permission.RecordAudio);
+      return res == PermissionStatus.authorized;
+    } else {
+      return true;
+    }
+  }
+
+    // Platform messages are asynchronous, so we initialize in an async method.
+  void activateSpeechRecognizer() async {
+    _speech = new SpeechRecognition();
+    // _speech.setAvailabilityHandler(onSpeechAvailability);
+    // _speech.setCurrentLocaleHandler(onCurrentLocale);
+    // _speech.setRecognitionStartedHandler(onRecognitionStarted);
+    // _speech.setRecognitionResultHandler(onRecognitionResult);
+    // _speech.setRecognitionCompleteHandler(onRecognitionComplete);
+    // _speech.setErrorHandler(errorHandler);
+
+    _voice_regcognition_permitted = await requirePermission();;
+    _speechRecognitionAvailable = await _speech.activate();
+  }
+
+  void startListening(Language lang) async {
+    var langCode = lang.code;
+
+    _isListening = true;
+    await _speech.listen(locale: langCode)
+  }
+
+  void cancelListening() async {
+    _isListening = false;
+    await _speech.cancel();
+  }
+
+  Future<String> finishListening() async {
+    var result = await _speech.stop();
+    return result;
+  }
+}
